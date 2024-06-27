3a97e4b9b18ba591bef92f2ff1ba18d2894e5b21
João Gabriel C. Helena <joaogchs5789@gmail.com>
2024-06-27 12:21:58 -0300

    Mode selection added

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ var path        = require('path'),
 
 const PLUGIN_NAME = 'gulp-tinify';
 
-function tinypng(file) {
+function tinypng(mode = ['image/webp']) {
     var validExtensions = ['.png', '.jpg', '.jpeg', '.webp'];
     if(!process.env.TINIFY_KEY) {
         throw new PluginError(PLUGIN_NAME, 'We can\'t upload images without your API Key. Looking for it in an enviroment variable named TINIFY_KEY');
     }
     tinify.key = process.env.TINIFY_KEY;
+
+
+    const validFormats = ['image/webp', 'image/jpg', 'image/png', 'image/jpeg'];
+    
+    // cant make it one if statement without making a long-ass comparisson
+    if (mode != 'direct') {
+        if (!Array.isArray(mode) || !mode.every(item => validFormats.includes(item))) {
+            throw new PluginError(PLUGIN_NAME, "Invalid formats array or value provided. Must be either a string with the value 'direct', to keep the filetype,\n    or an array only containing 'image/webp', 'image/jpg', 'image/png', 'image/jpeg'.\n    \u001b[4mhttps://tinypng.com/developers/reference/nodejs#converting-images\u001b[0m for more information on how arrays function.")
+        }
+    }
 
     return through.obj(async function(file, enc, cb) {
         if(file.isNull()) {
@@ -39,16 +49,27 @@ function tinypng(file) {
 
         const source = tinify.fromFile(file.path)
 
-        const converted = source.convert({ type: ["image/webp", "image/png"] });
-        const convertedExtension = await converted.result().extension()
-        converted.toBuffer(function(err, resultData) {
-            if(!err) {
-                gutil.log('Image processed: ' + file.basename + " -> " + gutil.replaceExtension(file.basename, '.' + convertedExtension))
-                file.contents = resultData;
-                file.path = gutil.replaceExtension(file.path, '.' + convertedExtension);
-            }
-            cb(null, file);
-        })
+        if (mode != 'direct') {
+            const converted = source.convert({ type: mode });
+            const convertedExtension = await converted.result().extension()
+            converted.toBuffer(function(err, resultData) {
+                if(!err) {
+                    gutil.log('Image processed: ' + file.basename + ' -> ' + gutil.replaceExtension(file.basename, '.' + convertedExtension))
+                    file.contents = resultData;
+                    file.path = gutil.replaceExtension(file.path, '.' + convertedExtension);
+                }
+                cb(null, file);
+            })
+        } else {
+            source.toBuffer(function(err, resultData) {
+                if(!err) {
+                    gutil.log('Image processed: ' + file.basename)
+                    file.contents = resultData;
+                }
+                cb(null, file);
+            })
+        }
+        
     });
 };
 // Exporting the plugin main function
